472fec623ae4beb3fa59bbd8c40bb64689b716f6
Mer Thich <97070315+programming-programmer@users.noreply.github.com>
2023-07-28 16:32:48 -0700

    Update remap.lua

--- .config/nvim/lua/mthich/remap.lua
+++ .config/nvim/lua/mthich/remap.lua
@@ -1,8 +1,9 @@
 -- WHERE"S THE MAP!@@!@#>>? HERE IT IS!!!!
 vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Yall got me FUNKED up if you think I'm reachin my teenie pinkie for that teeny escape key
-vim.keymap.set("i", "<C-g>", "<Esc>")
+vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- Sweet mother of magaldene, this brings a tear to my eye (also a pain in my back)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -18,6 +19,10 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
 -- WOOW REPLACE AANNNNDD EXEC. it must be heaven :)
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
@@ -27,6 +32,9 @@ vim.keymap.set("n", "<leader>tt", vim.cmd.tabnew)
 vim.keymap.set("n", "<leader>td", vim.cmd.tabclose)
 vim.keymap.set("n", "<leader>bb", vim.cmd.buffers)
 vim.keymap.set("n", "<leader>bd", vim.cmd.bdelete)
+
+-- The amazing (wow) tmux sessionizer in a simple keybind
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 --Because I'm also emacs-pilled. Save file like emacs
 vim.keymap.set("n", "<leader>fs", function()
