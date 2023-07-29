0ce5cea08dc6ee099bff0cc2a8f79cc7cd8ee04c
mthich <mthich@penguin>
2023-07-29 12:16:48 -0700

    Update Emacs and add ALL of the bindings. I am amazing ah ah amazing

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -2,3 +2,22 @@
 
 (package-initialize)
 (org-babel-load-file "~/.config/emacs/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(diminish deft latex magit rainbow-delimiters pdf-tools evil-tex org-roam org-superstar org-pomodoro evil-org counsel ivy-rich ivy general writeroom-mode visual-fill-column doom-themes which-key use-package undo-tree nyan-mode hide-mode-line evil-collection emojify doom-modeline)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fixed-pitch ((t (:family "Source Code Pro" :height 100))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.3))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.1))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.1))))
+ '(variable-pitch ((t (:family "Source Code Pro" :height 100 :weight medium)))))
