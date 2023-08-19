cdeb7801f5a890b06dbc6d876a8a9b22e269929f
programming-programmer <mthich15@gmail.com>
2023-08-19 11:31:07 -0700

    Remove all previous (jk). In favor of new ones (all the same)

--- .config/nvim/after/plugin/dap.lua
+++
@@ -1,5 +0,0 @@
-local dap = require("dap")
-
-vim.keymap.set('n', '<leader>b', function () dap.toggle_breakpoint() end)
-vim.keymap.set('n', '<leader>B', function () dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end)
-vim.keymap.set('n', '<leader>lp', function () dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
--- .config/nvim/lua/mthich/init.lua
+++
@@ -1,7 +0,0 @@
-require("mthich.remap")
-require("mthich.setup")
-
--- THEME
-local theme = require("nord")
-vim.g.nord_disable_background = true
-theme.set()
